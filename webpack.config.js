10090d42657a6f5de413d85be889d91931bcddb8
Howard Jiang <howard.yunghao.jiang@gmail.com>
2017-08-06 01:44:08 -0700

    eslint tuning, babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/**
+ * /webpack.config.js
+ */
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+module.exports = {
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve('dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader",
+          "eslint-loader",
+        ]
+      }
+    ],
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
